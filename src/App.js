5d1e32618de0771124d919508e40ec6274f1bb0b
Devin McAllester <devin.mcallester@pnnl.gov>
2019-09-05 16:59:01 -0700

    Reproduces issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      validationSchema={Yup.object().shape({
+        testVal: Yup.string().required()
+      })}
+      initialValues={{}}
+    >
+      {formikProps => (
+        <>
+          <button
+            onClick={() =>
+              formikProps
+                .submitForm()
+                .then(val => console.log("Promise resolved with: ", val))
+                .catch(error => console.log("Promise rejected with: ", error))
+            }
+          >
+            Submit
+          </button>
+          <pre
+            style={{
+              background: "#f6f8fa",
+              fontSize: ".65rem",
+              padding: ".5rem"
+            }}
+          >
+            <strong>props</strong> = {JSON.stringify(formikProps, null, 2)}
+          </pre>
+        </>
+      )}
+    </Formik>
   );
 }
 
